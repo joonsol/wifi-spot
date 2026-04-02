991ec9fd17a20e2cf559a3ae59f454cf84db7b3e
joonsol <71648638+joonsol@users.noreply.github.com>
2026-04-03 08:05:51 +0900

    data-processing

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
-
+import React, { useState, useMemo } from 'react'
+import wifiData from '../assets/wifi.json'
+import MapView from '../components/MapView'
 const MapPage = () => {
+  const [q, setQ] = useState('')
+
+  const filtered = useMemo(() => {
+    const keyword = q.trim()
+    if (!keyword) return wifiData.slice(0, 50)
+
+    return wifiData.
+      filter((x) => (
+        x.name + "" + x.detail
+      ).toLowerCase().includes(keyword.toLowerCase())).slice(0, 50)
+  }, [q])
   return (
     <div className='grid gap-4 lg:grid-cols-[1.4fr_0.6fr]  max-w-7xl mx-auto my-3'>
       {/* 지도영역 */}
@@ -10,7 +22,7 @@ const MapPage = () => {
           <p className='text-xs text-slate-500'>내 주변 공공 와이파이</p>
         </div>
         <div className='h-[70vh] bg-slate-100'>
-          지도 들어갈 영역
+          <MapView/>
         </div>
       </section>
       {/* 리스트영역 */}
@@ -21,23 +33,24 @@ const MapPage = () => {
         </div>
         <div className='flex gap-2 border-b px-4 py-3'>
           <input
+            value={q}
+            onChange={(e) => setQ(e.target.value)}
             className='flex-3 rounded-lg border px-3 py-2 text-sm outline-none focus:ring-2 focus:ring-slate-900/20'
             type="text" placeholder='장소 주소 검색' />
-          <button className='rounded-lg bg-slate-900 text-sm text-white flex-1'>검색</button>
-        </div>
+         </div>
         {/* 목록 더미 */}
         <ul className='max-h-[60vh] overflow-auto p-2'>
-          {Array.from({ length: 15 }).map((_, idx) => (
+          {filtered.map((item, idx) => (
             <li
               key={idx}
               className='rounded-xl p-3 hover:bg-slate-50 cursor-pointer'
             >
               <div className='flex items-start justify-between gap-3'>
                 <div>
-                  <div className='text-sm font-semibold'>Public Wifi Spot #{idx + 1}</div>
-                  <div className='mt-1 text-xs text-slate-500'>서울시 어딘가 주소 예시</div>
+                  <div className='text-sm font-semibold'>{item.name}</div>
+                  <div className='mt-1 text-xs text-slate-500'>{item.detail}</div>
                 </div>
-                <div className='rounded-full bg-slate-100 px-2 text-xs text-slate-600'>0.{idx}km</div>
+                <div className='rounded-full bg-slate-100 px-2 text-xs text-slate-600'>{item.phone}</div>
               </div>
             </li>
           ))}
